4357cc7dc57e656e660d95b35b845e34efd4f2c7
Paul Götze <paul.christoph.goetze@gmail.com>
2015-11-21 00:07:15 +0100

    Add #internal_value_of to Attribute

--- lib/weka/core/attribute.rb
+++ lib/weka/core/attribute.rb
@@ -7,6 +7,18 @@ module Weka
       def values
         enumerate_values.to_a
       end
+
+      # The order of the if statements is important here, because a date is also
+      # a numeric.
+      def internal_value_of(value)
+        if date?
+          parse_date(value)
+        elsif numeric?
+          value
+        elsif nominal?
+          index_of_value(value)
+        end
+      end
     end
   end
 end
--- spec/core/attribute_spec.rb
+++ spec/core/attribute_spec.rb
@@ -6,10 +6,29 @@ describe Weka::Core::Attribute do
   subject { Weka::Core::Attribute.new('class', values) }
 
   it { is_expected.to respond_to :values }
+  it { is_expected.to respond_to :internal_value_of }
 
   describe '#values' do
     it 'should return an array of the values' do
       expect(subject.values).to eq values
+    end
+  end
+
+  describe '#internal_value_of' do
+    it 'should return the value for a numeric attribute' do
+      attribute = Weka::Core::Attribute.new('attribute name')
+      expect(attribute.internal_value_of(3.5)).to eq 3.5
+    end
+
+    it 'should return the internal index for a nominal attribute' do
+      attribute = Weka::Core::Attribute.new('class', ['yes', 'no'])
+      expect(attribute.internal_value_of('yes')).to eq 0
+      expect(attribute.internal_value_of('no')).to  eq 1
+    end
+
+    it 'should return the date number value for a data attribute' do
+      attribute = Weka::Core::Attribute.new('date', 'yyyy-MM-dd HH:mm')
+      expect(attribute.internal_value_of('2015-12-24 11:11')).to eq 1450951860000.0
     end
   end
 end
