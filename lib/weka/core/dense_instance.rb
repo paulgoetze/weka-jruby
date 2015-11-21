894abe1018eedc1e4dec357fd4c3afde97686316
Paul Götze <paul.christoph.goetze@gmail.com>
2015-11-21 01:40:49 +0100

    Add basic methods and aliases for DenseInstance

--- lib/weka/core/dense_instance.rb
+++ lib/weka/core/dense_instance.rb
@@ -1,9 +1,48 @@
 module Weka
   module Core
     java_import "weka.core.DenseInstance"
-    java_import "weka.core.FastVector"
 
     class DenseInstance
+      java_import "java.util.Date"
+      java_import "java.text.SimpleDateFormat"
+
+      def attributes
+        enumerate_attributes.to_a
+      end
+
+      def each_attribute(&block)
+        enumerate_attributes.each { |attribute| yield(attribute) }
+      end
+
+      def each_attribute_with_index(&block)
+        enumerate_attributes.each_with_index do |attribute, index|
+          yield(attribute, index)
+        end
+      end
+
+      def to_a
+        to_double_array.each_with_index.map do |value, index|
+          attribute = attributes[index]
+
+          if attribute.date?
+            format_date(value, attribute.date_format)
+          elsif attribute.numeric?
+            value
+          elsif attribute.nominal?
+            attribute.value(value)
+          end
+        end
+      end
+
+      alias :values       :to_a
+      alias :values_count :num_values
+
+      private
+
+      def format_date(value, format)
+        formatter = SimpleDateFormat.new(format)
+        formatter.format(Date.new(value))
+      end
     end
   end
 end
---
+++ spec/core/dense_instance_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+describe Weka::Core::DenseInstance do
+
+  let(:file) { File.expand_path('../../support/resources/weather.arff', __FILE__) }
+
+  subject do
+    instances = Weka::Core::Parser.parse_arff(file)
+    instances.add_date_attribute('recorded_at')
+    instances.add_instance(['rainy',50, 50,'TRUE','no','2015-12-24 11:11'])
+    instances.instances.last
+  end
+
+  it { is_expected.to respond_to :to_a }
+  it { is_expected.to respond_to :attributes }
+  it { is_expected.to respond_to :each_attribute }
+  it { is_expected.to respond_to :each_attribute_with_index }
+
+  describe 'aliases:' do
+    {
+      values:       :to_a,
+      values_count: :num_values
+    }.each do |method, alias_method|
+      it "should define the alias ##{alias_method} for ##{method}" do
+        expect(subject.method(method)).to eq subject.method(alias_method)
+      end
+    end
+  end
+
+  describe '#to_a' do
+    it 'should return an Array with the values of the instance' do
+      values = ['rainy',50.0, 50.0,'TRUE','no','2015-12-24 11:11']
+      expect(subject.to_a).to eq values
+    end
+  end
+
+  describe '#attributes' do
+    it 'should return an Array of Attributes' do
+      attributes = subject.attributes
+      expect(attributes).to be_an Array
+
+      all_kind_of_attribute = attributes.reduce(true) do |result, attribute|
+        result &&= attribute.kind_of?(Java::WekaCore::Attribute)
+      end
+
+      expect(all_kind_of_attribute).to be true
+    end
+  end
+
+  describe 'enumerator' do
+    before { @result = nil }
+
+    describe '#each_attribute' do
+      it 'should run a block on each attribute' do
+        subject.each_attribute do |attribute|
+          @result = attribute.name unless @result
+        end
+
+        expect(@result).to eq 'outlook'
+      end
+    end
+
+    describe '#each_attribute_with_index' do
+      it 'should run a block on each attribute' do
+        subject.each_attribute_with_index do |attribute, index|
+          @result = "#{attribute.name}, #{index}" if index == 0
+        end
+
+        expect(@result).to eq 'outlook, 0'
+      end
+    end
+  end
+
+end
