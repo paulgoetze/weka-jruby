ce136a334c6a410b3ccbf8236674676e1884677f
Paul Götze <paul.christoph.goetze@gmail.com>
2015-11-26 22:40:43 +0100

    Build converters with class builder

--- lib/weka/core/converters.rb
+++ lib/weka/core/converters.rb
@@ -1,14 +1,18 @@
+require 'weka/class_builder'
+
 module Weka
   module Core
     module Converters
-      java_import 'java.io.File'
-      java_import 'weka.core.converters.ArffLoader'
-      java_import 'weka.core.converters.CSVLoader'
-      java_import 'weka.core.converters.JSONLoader'
+      include ClassBuilder
 
-      java_import 'weka.core.converters.ArffSaver'
-      java_import 'weka.core.converters.CSVSaver'
-      java_import 'weka.core.converters.JSONSaver'
+      java_import 'java.io.File'
+
+      build_classes :ArffLoader,
+                    :ArffSaver,
+                    :CSVLoader,
+                    :CSVSaver,
+                    :JSONLoader,
+                    :JSONSaver
     end
   end
 end
---
+++ spec/core/converters_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Weka::Core::Converters do
+
+  it_behaves_like 'class builder'
+
+  [
+    :ArffLoader,
+    :ArffSaver,
+    :CSVLoader,
+    :CSVSaver,
+    :JSONLoader,
+    :JSONSaver
+  ].each do |class_name|
+    it "should define a class #{class_name}" do
+      expect(described_class.const_defined?(class_name)).to be true
+    end
+  end
+end
