6eca10cffe11bc24e339b59e7a93b63fcc1da193
Paul Götze <paul.christoph.goetze@gmail.com>
2015-12-07 23:41:32 +0100

    Add classifier utils

---
+++ lib/weka/classifiers/utils.rb
@@ -0,0 +1,31 @@
+require 'active_support/concern'
+require 'weka/core/dense_instance'
+
+module Weka
+  module Classifiers
+    module Utils
+      extend ActiveSupport::Concern
+
+      included do
+        def self.build(&block)
+          instance = new
+          instance.instance_eval(&block) if block_given?
+          instance
+        end
+
+        if instance_methods.include?(:build_classifier)
+          alias :train_with_instances :build_classifier
+        end
+
+        if instance_methods.include?(:update_classifier)
+          alias :add_training_instance :update_classifier
+        end
+
+        if self.respond_to?(:__persistent__=)
+          self.__persistent__ = true
+        end
+      end
+
+    end
+  end
+end
