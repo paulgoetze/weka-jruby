7ede9dd80809d875df944b6a078a112f3511f243
Paul Götze <paul.christoph.goetze@gmail.com>
2015-12-07 06:32:46 +0100

    Require utils.rb first if available

--- lib/weka.rb
+++ lib/weka.rb
@@ -5,9 +5,22 @@ require 'weka/version'
 module Weka
   include Jars
 
-  def self.require_all(type)
-    files = Dir[File.expand_path("../weka/#{type}/**/*.rb", __FILE__)]
-    files.each { |file| require file }
+  class << self
+    def require_all(type)
+      files        = Dir[File.expand_path("../weka/#{type}/**/*.rb", __FILE__)]
+      utils        = File.expand_path("../weka/#{type}/utils.rb", __FILE__)
+      sorted_files = move_to_head(utils, files)
+
+      sorted_files.each { |file| require file }
+    end
+
+    private
+
+    def move_to_head(file, files)
+      file_to_move = files.delete(file)
+      files.unshift(file_to_move) unless file_to_move.nil?
+      files
+    end
   end
 end
 
