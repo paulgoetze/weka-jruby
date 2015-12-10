008980419b9dc2e1d3b927ae2f3f0881cd72a669
Paul Götze <paul.christoph.goetze@gmail.com>
2015-12-10 22:30:42 +0100

    Add #cross_validate and #training_instances to classifiers utils

--- lib/weka/classifiers/utils.rb
+++ lib/weka/classifiers/utils.rb
@@ -1,4 +1,5 @@
 require 'active_support/concern'
+require 'weka/classifiers/evaluation'
 
 module Weka
   module Classifiers
@@ -6,8 +7,30 @@ module Weka
       extend ActiveSupport::Concern
 
       included do
+        java_import 'java.util.Random'
+
         if instance_methods.include?(:build_classifier)
-          alias :train_with_instances :build_classifier
+          attr_reader :training_instances
+
+          def train_with_instances(instances)
+            @training_instances = instances
+            build_classifier(instances)
+
+            self
+          end
+
+          def cross_validate(folds: 3)
+            evaluation = Evaluation.new(self.training_instances)
+
+            evaluation.cross_validate_model(
+              self,
+              self.training_instances,
+              folds.to_i.to_java(:int),
+              Java::JavaUtil::Random.new(1)
+            )
+
+            evaluation
+          end
         end
 
         if instance_methods.include?(:update_classifier)
--- spec/classifiers/utils_spec.rb
+++ spec/classifiers/utils_spec.rb
@@ -4,26 +4,30 @@ describe Weka::Classifiers::Utils do
 
   let(:including_class) do
     Class.new do
-      def build_classifier
+      def build_classifier(instances)
+      end
+
+      def update_classifier(instance)
       end
 
       include Weka::Classifiers::Utils
     end
   end
 
-  subject { including_class.new }
+  subject { including_class.new.train_with_instances(instances) }
+
+  let(:instances) do
+    file      = File.expand_path('./../../support/resources/weather.arff', __FILE__)
+    instances = Weka::Core::Instances.from_arff(file)
+
+    instances.class_attribute = :play
+    instances
+  end
 
   it { is_expected.to respond_to :train_with_instances }
-
-  describe 'aliases:' do
-    {
-      train_with_instances:  :build_classifier
-    }.each do |alias_method, method|
-      it "should define the alias ##{alias_method} for ##{method}" do
-        expect(subject.method(method)).to eq subject.method(alias_method)
-      end
-    end
-  end
+  it { is_expected.to respond_to :training_instances }
+  it { is_expected.to respond_to :add_training_instance }
+  it { is_expected.to respond_to :cross_validate }
 
   describe 'if included' do
     subject { Class.new }
@@ -31,6 +35,49 @@ describe Weka::Classifiers::Utils do
     it 'should set __persistent__ to true' do
       expect(subject).to receive(:__persistent__=).with(true).once
       subject.include(Weka::Classifiers::Utils)
+    end
+  end
+
+  describe '#train_with_instances' do
+    it 'should call Java‘s #build_classifier' do
+      expect(subject)
+        .to receive(:build_classifier).once
+        .with(instances)
+
+      subject.train_with_instances(instances)
+    end
+
+    it 'should set the training_instances' do
+      subject = including_class.new
+      expect(subject.training_instances).to be_nil
+      subject.train_with_instances(instances)
+      expect(subject.training_instances).to eq instances
+    end
+
+    it 'should return itself' do
+      expect(subject.train_with_instances(instances)).to be subject
+    end
+  end
+
+  describe '#cross_validate' do
+    let(:folds) { 2 }
+
+    before do
+      allow(subject).to receive(:training_instances).and_return(instances)
+      allow_any_instance_of(Weka::Classifiers::Evaluation)
+        .to receive(:cross_validate_model)
+    end
+
+    it 'should return a Weka::Classifiers::Evaluation' do
+      return_value = subject.cross_validate(folds: folds)
+      expect(return_value).to be_kind_of Weka::Classifiers::Evaluation
+    end
+
+    it 'should run Java‘s #cross_validate_model on an Evaluation' do
+      expect_any_instance_of(Weka::Classifiers::Evaluation)
+        .to receive(:cross_validate_model).once
+
+      subject.cross_validate(folds: folds)
     end
   end
 
