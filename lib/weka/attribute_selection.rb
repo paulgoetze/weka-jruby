00ae9a3e62665a224073d25a035a0a66e42f21a4
Paul Götze <paul.christoph.goetze@gmail.com>
2015-12-19 16:08:38 +0100

    Move AttributeSelection class definition to module directory
    Instead of defining the class, /weka/attribute_selection.rb now only
    requires all files from /weka/attribute_selection/.

--- lib/weka/attribute_selection.rb
+++ lib/weka/attribute_selection.rb
@@ -1,14 +1,1 @@
-require 'weka/attribute_selection/evaluator'
-require 'weka/attribute_selection/search'
-
-module Weka
-  module AttributeSelection
-    java_import 'weka.attributeSelection.AttributeSelection'
-
-    class AttributeSelection
-
-      alias :summary :to_results_string
-      alias :selected_attributes_count :number_attributes_selected
-    end
-  end
-end
+Weka.require_all :attribute_selection
---
+++ lib/weka/attribute_selection/attribute_selection.rb
@@ -0,0 +1,11 @@
+module Weka
+  module AttributeSelection
+    java_import 'weka.attributeSelection.AttributeSelection'
+
+    class AttributeSelection
+
+      alias :summary :to_results_string
+      alias :selected_attributes_count :number_attributes_selected
+    end
+  end
+end
