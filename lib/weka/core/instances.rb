7a2b732b308496e06d05a1c02ff97ccada96ce6e
Kai-Chun Ning <kaichun.ning@gmail.com>
2016-12-23 04:31:51 +0100

    Instances:         1) refactor #attribute_values_to_hash         2) rewrite comment

--- lib/weka/core/instances.rb
+++ lib/weka/core/instances.rb
@@ -55,7 +55,7 @@ module Weka
 
       def attribute_names(use_symbol = false)
         if use_symbol
-          attributes.map(&:name).map(&:to_sym)
+          attributes.map { |attribute| attribute.name.to_sym }
         else
           attributes.map(&:name)
         end
@@ -195,19 +195,21 @@ module Weka
 
       # Add new instance
       # @param [Instance, Array, Hash] instance_or_values the attribute values
-      #   of the instance to be added
+      #   of the instance to be added. If passing an array, the attribute values
+      #   must be in the same order as the attributes defined in Instances.
+      #   If passing a hash, The keys are the names of the attributes and their
+      #   values are corresponding attributes values.
       #
       # @example Passing Instance
       #   instances.add_instance(instance)
       #
-      # @example Passing an array of attribute values. Note that the attribute
-      #   values must be in the same order as the Instances attributes.
-      #   instances.add_instance([attr1_value, attr2_value, attr3_value, ...])
+      # @example Passing an array of attribute values
+      #   attribute_values = [attr1_value, attr2_value, attr3_value, ...]
+      #   instances.add_instance(attribute_values)
       #
-      # @example Passing a hash of attribute values. The keys are the names of
-      #   the attributes and their values are corresponding attribute values.
-      #   hash = { attr1_name: attr1_value, attr2_name: attr2_value, ... }
-      #   instances.add_instance(hash)
+      # @example Passing a hash of attribute values.
+      #   attribute_values = { attr1_name: attr1_value, attr2_name: attr2_value, ... }
+      #   instances.add_instance(attribute_values)
       #
       def add_instance(instance_or_values, weight: 1.0)
         instance = instance_from(instance_or_values, weight: weight)
@@ -319,7 +321,7 @@ module Weka
         Attribute.const_get(type.upcase)
       end
 
-      # Map a hash whose keys are attribute names and values are attribute
+      # convert a hash whose keys are attribute names and values are attribute
       #   values into an array containing attribute values in the order
       #   of the Instances attributes.
       #
@@ -334,7 +336,7 @@ module Weka
         end
       end
 
-      # Map an array of attribute values in the same order as Instances
+      # convert an array of attribute values in the same order as Instances
       #   attributes into a hash whose keys are attribute names and values
       #   are corresponding attribute values.
       #
@@ -343,12 +345,10 @@ module Weka
       #
       # @return [Hash] a hash as described above
       def attribute_values_to_hash(attribute_values)
-        # TODO: make this pretty
-        hash = {}
-        attribute_names(true).each_with_index do |attribute_name, index|
-          hash[attribute_name] = attribute_values[index]
+        attribute_names(true).each_with_index.inject({}) do |hash, (attr_name, index)|
+          hash[attr_name] = attribute_values[index]
+          hash
         end
-        hash
       end
     end
 
