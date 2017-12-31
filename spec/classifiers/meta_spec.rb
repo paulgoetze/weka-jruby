990b44b5e36716d6d11dacd62982068b53939b0a
Paul Götze <paul.christoph.goetze@gmail.com>
2017-12-31 11:12:50 +0100

    Make WeightedInstancesHandlerWrapper available in Weka::Classifiers::Meta

--- spec/classifiers/meta_spec.rb
+++ spec/classifiers/meta_spec.rb
@@ -23,6 +23,7 @@ describe Weka::Classifiers::Meta do
     RegressionByDiscretization
     Stacking
     Vote
+    WeightedInstancesHandlerWrapper
   ].each do |class_name|
     it "defines a class #{class_name}" do
       expect(described_class.const_defined?(class_name)).to be true
